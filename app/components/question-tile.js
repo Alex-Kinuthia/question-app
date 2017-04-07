df80f2d90075ec997ec81e761ba12fb0ea878183
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-04-07 18:19:35 +0300

    Fix syntax errors

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,20 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-      update(question, params) {
-        this.sendAction('update', question, params);
-      },
-      like(question) {
-        if (confirm('You have liked the question')) {
-          this.sendAction('likeQuestion', question);
-        },
-        hate(question) {
-          if (confirm('You have hated the question')) {
-            this.sendAction('hateQuestion', question);
-          }
-          delete(question) {
-            if (confirm('Are you sure you want to delete this question?')) {
-              this.sendAction('destroyQuestion', question);
-            }
-          }
-        });
+  update(question, params) {
+    this.sendAction('update', question, params);
+  },
+  like(question) {
+    if (confirm('You have liked the question')) {
+      this.sendAction('likeQuestion', question);
+    }
+  },
+  hate(question) {
+    if (confirm('You have hated the question')) {
+      this.sendAction('hateQuestion', question);
+    }
+  },
+  delete(question) {
+    if (confirm('Are you sure you want to delete this question?')) {
+      this.sendAction('destroyQuestion', question);
+    }
+  }
+});
