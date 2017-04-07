7f8639f0b79126f759f2b1105762700f7ee549d5
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-04-07 08:18:46 +0300

    updated the index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
+var rentals = [{
+  id: 1,
+  author: "Veruca Salt",
+  city: "San Francisco",
+  type: "Education",
+  question: "What are the 4 principles of matter?"
+}, {
+  id: 2,
+  author: "Mike TV",
+  city: "Seattle",
+  type: "Entertainment",
+  question: "Why do Kenyan djs not play Kenyan music?"
+}, {
+  id: 3,
+  author: "Violet Beauregarde",
+  city: "Portland",
+  type: "Life",
+  question: "Why do people give up in life?"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
