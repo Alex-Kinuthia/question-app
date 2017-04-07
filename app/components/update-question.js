ff639b06a7341d316355067488f31599fc334c99
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-04-07 13:51:10 +0300

    added contents in the index.js

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        author: this.get('author'),
+        type: this.get('type'),
+        city: this.get('city'),
+        question: this.get('question'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,17 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
-
+  // enables user to save
   actions: {
     saveQuestion3(params) {
       var newRQuestion = this.store.createRecord('question', params);
       newQuestion.save();
+      this.transitionTo('index');
+    },
+    // user to update
+    update(question, params) {
+      debugger;
+      question.save();
       this.transitionTo('index');
     },
     // user to be able to delete
